10b7e76c0ebc1c0bf87f468436657c3f5480867a
Leah Hill <leah@verysandwich.com>
2018-07-12 17:23:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 const playlist = {nilsson: 'one', Blur: 'song 2', KingstonTrio: 'Three Song'}
 
 function updatePlaylist(playlist, artist, song) {
-  playlist[artist]=song
+  playlist[artist]=song;
+  return playlist
 }
 
 function updateObjectWithKeyAndValue(obj, key, value) {
