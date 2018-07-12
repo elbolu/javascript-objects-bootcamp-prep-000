7b6327dc626e76146de2d42168580eaafd51f870
Leah Hill <leah@verysandwich.com>
2018-07-12 17:15:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-const playlist = {artist: 'song', Blur: 'song 2', {
+const playlist = {nilsson: 'one', Blur: 'song 2', {
 
 function updatePlaylist(playlist, artist, song) {
   return Object.assign({}, playlist, { [artist]: song })
